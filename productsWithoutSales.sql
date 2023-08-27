551114a481007c43cb068be7a6bd31046b89d1c7
Vaneeza-7 <88858636+Vaneeza-7@users.noreply.github.com>
2023-08-27 13:32:49 +0500

    intermediate test

--- productsWithoutSales.sql
+++ productsWithoutSales.sql
@@ -1,2 +1,8 @@
 --find all the products that were not sold
-
+go
+select product.sku, product.product_name
+from product
+where product.id not in (
+  select invoice_item.product_id from invoice_item
+  )
+order by product.sku asc
