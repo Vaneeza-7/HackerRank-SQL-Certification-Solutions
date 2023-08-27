002cf0e1ed73101113c73d0c6cede0d6d473cc08
Vaneeza-7 <88858636+Vaneeza-7@users.noreply.github.com>
2023-08-27 13:28:33 +0500

    intermediate test

--- businessExpansion.sql
+++ businessExpansion.sql
@@ -1,2 +1,8 @@
 --write a query to find all pairs of customers who have been in contact more than once.
-
+go
+select user_account_id, user_account.first_name, user_account.last_name, customer_id, customer.customer_name, count(*)
+from contact
+inner join user_account on contact.user_account_id=user_account.id
+inner join customer on customer.id=contact.customer_id
+group by user_account_id, customer_id, user_account.first_name, user_account.last_name, customer.customer_name
+having count(*) > 1
